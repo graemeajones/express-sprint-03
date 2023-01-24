4f9a7b4c7db1abf07f8b321d5aa5e7b33b9a638c
Graeme Jones <48164351+graemeajones@users.noreply.github.com>
2023-01-24 17:26:27 +0000

    Accessor class implemented (#15)
    Co-authored-by: GraemeAngusJones <86078388+GraemeAngusJones@users.noreply.github.com>

--- routers/modules-router.js
+++ routers/modules-router.js
@@ -2,121 +2,66 @@ import { Router } from 'express';
 import Model from '../models/Model.js';
 import modelConfig from '../models/modules-model.js';
 import database from '../database.js';
+import Accessor from '../accessor/Accessor.js';
 
 // Model -----------------------------------------
 
 const model = new Model(modelConfig);
 
-// Data accessors --------------------------------
+// Data accessor ---------------------------------
 
-const create = async (record) => {
-  try {
-    const { sql, data } = model.buildCreateQuery(record); 
-    const status = await database.query(sql, data);
-
-    const { isSuccess, result, message } = await read(status[0].insertId, null);
-    return isSuccess
-      ? { isSuccess: true, result: result, message: 'Record successfully recovered' }
-      : { isSuccess: false, result: null, message: `Failed to recover the inserted record: ${message}` };
-  }
-  catch (error) {
-    return { isSuccess: false, result: null, message: `Failed to execute query: ${error.message}` };
-  }
-};
-
-const read = async (id, variant) => {
-  try {
-    const { sql, data } = model.buildReadQuery(id, variant);
-    const [result] = await database.query(sql, data);
-    return (result.length === 0)
-      ? { isSuccess: false, result: null, message: 'No record(s) found' }
-      : { isSuccess: true, result: result, message: 'Record(s) successfully recovered' };
-  }
-  catch (error) {
-    return { isSuccess: false, result: null, message: `Failed to execute query: ${error.message}` };
-  }
-};
-
-const update = async (record, id) => {
-  try {
-    const { sql, data } = model.buildUpdateQuery(record, id);
-    const status = await database.query(sql, data);
-    if (status[0].affectedRows === 0)
-      return { isSuccess: false, result: null, message: 'Failed to update record: no rows affected' };
-
-    const { isSuccess, result, message } = await read(id, null);      
-    return isSuccess
-      ? { isSuccess: true, result: result, message: 'Record successfully recovered' }
-      : { isSuccess: false, result: null, message: `Failed to recover the updated record: ${message}` };
-  }
-  catch (error) {
-    return { isSuccess: false, result: null, message: `Failed to execute query: ${error.message}` };
-  }
-};
-
-const _delete = async (id) => {
-  try {
-    const { sql, data } = model.buildDeleteQuery(id);
-    const status = await database.query(sql, data);
-    return status[0].affectedRows === 0
-      ? { isSuccess: false, result: null, message: `Failed to delete record ${id}` }
-      : { isSuccess: true, result: null, message: 'Record successfully deleted' };
-  }
-  catch (error) {
-    return { isSuccess: false, result: null, message: `Failed to execute query: ${error.message}` };
-  }
-};
+const accessor = new Accessor(model, database);
 
 // Controllers -----------------------------------
 
-const getModulesController = async (req, res, variant) => {
+const getController = async (req, res, variant) => {
   const id = req.params.id;
 
   // Validate request
 
   // Access data
-  const { isSuccess, result, message: accessorMessage } = await read(id, variant);
+  const { isSuccess, result, message: accessorMessage } = await accessor.read(id, variant);
   if (!isSuccess) return res.status(404).json({ message: accessorMessage });
   
   // Response to request
   res.status(200).json(result);
 };
 
-const postModulesController = async (req, res) => {
+const postController = async (req, res) => {
   const record = req.body;
   
   // Validate request
 
   // Access data
-  const { isSuccess, result, message: accessorMessage } = await create(record);
+  const { isSuccess, result, message: accessorMessage } = await accessor.create(record);
   if (!isSuccess) return res.status(400).json({ message: accessorMessage });
   
   // Response to request
   res.status(201).json(result);
 };
 
-const putModulesController = async (req, res) => {
+const putController = async (req, res) => {
   const id = req.params.id;
   const record = req.body;
 
   // Validate request
 
   // Access data
-  const { isSuccess, result, message: accessorMessage } = await update(record, id);
+  const { isSuccess, result, message: accessorMessage } = await accessor.update(record, id);
   if (!isSuccess) return res.status(400).json({ message: accessorMessage });
   
   // Response to request
   res.status(200).json(result);
 };
 
-const deleteModulesController = async (req, res) => {
+const deleteController = async (req, res) => {
   const id = req.params.id;
 
   // Validate request
 
   // Access data
   
-  const { isSuccess, result, message: accessorMessage } = await _delete(id);
+  const { isSuccess, result, message: accessorMessage } = await accessor.delete(id);
   if (!isSuccess) return res.status(400).json({ message: accessorMessage });
   
   // Response to request
@@ -127,12 +72,12 @@ const deleteModulesController = async (req, res) => {
 
 const router = new Router();
 
-router.get('/', (req, res) => getModulesController(req, res, null));
-router.get('/:id(\\d+)', (req, res) => getModulesController(req, res, null));
-router.get('/leader/:id', (req, res) => getModulesController(req, res, 'leader'));
-router.get('/users/:id', (req, res) => getModulesController(req, res, 'users'));
-router.post('/', postModulesController);
-router.put('/:id', putModulesController);
-router.delete('/:id', deleteModulesController);
+router.get('/', (req, res) => getController(req, res, null));
+router.get('/:id(\\d+)', (req, res) => getController(req, res, null));
+router.get('/leader/:id', (req, res) => getController(req, res, 'leader'));
+router.get('/users/:id', (req, res) => getController(req, res, 'users'));
+router.post('/', postController);
+router.put('/:id', putController);
+router.delete('/:id', deleteController);
 
 export default router;
