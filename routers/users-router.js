e7d2957efd3e837c867f573761b8f458dceae468
GraemeAngusJones <86078388+GraemeAngusJones@users.noreply.github.com>
2023-01-24 20:22:54 +0000

    Controller class implemented

--- routers/users-router.js
+++ routers/users-router.js
@@ -3,6 +3,7 @@ import Model from '../models/Model.js';
 import modelConfig from '../models/users-model.js';
 import database from '../database.js';
 import Accessor from '../accessor/Accessor.js';
+import Controller from '../controller/Controller.js';
 
 // Model -----------------------------------------
 
@@ -12,72 +13,21 @@ const model = new Model(modelConfig);
 
 const accessor = new Accessor(model, database);
 
-// Controllers -----------------------------------
+// Controller ------------------------------------
 
-const getController = async (req, res, variant) => {
-  const id = req.params.id;
-
-  // Validate request
-
-  // Access data
-  const { isSuccess, result, message: accessorMessage } = await accessor.read(id, variant);
-  if (!isSuccess) return res.status(404).json({ message: accessorMessage });
-  
-  // Response to request
-  res.status(200).json(result);
-};
-
-const postController = async (req, res) => {
-  const record = req.body;
-
-  // Validate request
-
-  // Access data
-  const { isSuccess, result, message: accessorMessage } = await accessor.create(record);
-  if (!isSuccess) return res.status(400).json({ message: accessorMessage });
-  
-  // Response to request
-  res.status(201).json(result);
-};
-
-const putController = async (req, res) => {
-  const id = req.params.id;
-  const record = req.body;
-
-  // Validate request
-
-  // Access data
-  const { isSuccess, result, message: accessorMessage } = await accessor.update(record, id);
-  if (!isSuccess) return res.status(400).json({ message: accessorMessage });
-  
-  // Response to request
-  res.status(200).json(result);
-};
-
-const deleteController = async (req, res) => {
-  const id = req.params.id;
-
-  // Validate request
-
-  // Access data
-  const { isSuccess, result, message: accessorMessage } = await accessor.delete(id);
-  if (!isSuccess) return res.status(400).json({ message: accessorMessage });
-  
-  // Response to request
-  res.status(204).json({ message: accessorMessage });
-};
+const controller = new Controller(accessor);
 
 // Endpoints -------------------------------------
 
 const router = new Router();
 
-router.get('/', (req, res) => getController(req, res, null));
-router.get('/:id(\\d+)', (req, res) => getController(req, res, null));
-router.get('/student', (req, res) => getController(req, res, 'student'));
-router.get('/staff', (req, res) => getController(req, res, 'staff'));
-router.get('/groups/:id', (req, res) => getController(req, res, 'groups'));
-router.post('/', postController);
-router.put('/:id', putController);
-router.delete('/:id', deleteController);
+router.get('/', (req, res) => controller.get(req, res, null));
+router.get('/:id(\\d+)', (req, res) => controller.get(req, res, null));
+router.get('/student', (req, res) => controller.get(req, res, 'student'));
+router.get('/staff', (req, res) => controller.get(req, res, 'staff'));
+router.get('/groups/:id', (req, res) => controller.get(req, res, 'groups'));
+router.post('/', controller.post);
+router.put('/:id', controller.put);
+router.delete('/:id', controller.delete);
 
 export default router;
